6aad3fe9df35f833b8a3817bd13fa344cf86ca49
onejemee <>
2021-05-19 17:32:23 +0000

    hello world

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	let project = " Hello World! "
-  console.log(project);
+	let project = "Hello World!"
+  console.log(JSON.stringify(project));
 }
  
 module.exports = hello;
